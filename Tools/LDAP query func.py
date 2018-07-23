fb83c289a5930e007b435805db15eae95583c4f6
Rozhdestvenskiy Dmitriy <dremsama@gmail.com>
2018-07-23 18:20:26 +0300

    new func added

--- Tools/LDAP query func.py
+++ Tools/LDAP query func.py
@@ -3,11 +3,12 @@ import ldap3.core.exceptions as exceptions
 import Configuration
 import logging
 
-requested_hostname = 'vibmpc0ej3mc'
+#requested_hostname = 'vibmpc0ej3mc'
 ldap_conf = Configuration.LdapConfig()
 ldap_server = Server(ldap_conf.ad_server, get_info=ALL)
 CONN_TO_LDAP = Connection(ldap_server, user=ldap_conf.username, password=ldap_conf.password, authentication=NTLM,
                   auto_bind=True)
+print('Connected to', CONN_TO_LDAP, 'as', CONN_TO_LDAP.extend.standard.who_am_i())
 
 
 def get_ad_host_description(connection_to_ldap,  requested_hostname: str) -> str:
@@ -25,6 +26,22 @@ def get_ad_host_description(connection_to_ldap,  requested_hostname: str) -> str
         return 'unknown'
 
 
-result = get_ad_host_description(connection_to_ldap=CONN_TO_LDAP, requested_hostname=requested_hostname)
+def get_ad_user(connection_to_ldap, user_name):
+        logger = logging.getLogger('root')
+        try:
+            # print('Connected to', connection_to_ldap, 'as', connection_to_ldap.extend.standard.who_am_i())
+            obj_user = ObjectDef('user', connection_to_ldap)
+            r = Reader(CONN_TO_LDAP, obj_user, 'DC=amust,DC=local', '(&(sAMAccountName=' + user_name + '))')
+            r.search()
+            user_info = r
+            return user_info
+        except Exception as error:
+            logger.error(error)
+            return 'unknown'
 
-print(result)
+
+#result = get_ad_host_description(connection_to_ldap=CONN_TO_LDAP, requested_hostname=requested_hostname)
+user_info = get_ad_user(connection_to_ldap=CONN_TO_LDAP, user_name='todd.snively')
+for user in user_info:
+    print(user.CN)
+    #print(user.userPrincipalName, ':', user.distinguishedName, ';', user.title, '---', user)
